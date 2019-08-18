feeec1fa420c69b19c6b3ef18ede7b5411d946fe
Anthony Navarro <Anthonarro@gmail.com>
2019-08-18 16:12:41 -0700

    Initial commit

---
+++ DinnerWithLucy/Data/DinnerWithLucyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DinnerWithLucy.Models
+{
+    public class DinnerWithLucyContext : DbContext
+    {
+        public DinnerWithLucyContext (DbContextOptions<DinnerWithLucyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DinnerWithLucy.Models.Recipe> Recipe { get; set; }
+    }
+}
